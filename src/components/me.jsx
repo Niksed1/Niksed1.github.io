8c3b526dffc0c739cb7bd11a6cce9444286e68a9
Niksed1 <image22061@gmail.com>
2025-01-04 15:23:28 -0800

    removed spring

--- src/components/me.jsx
+++ src/components/me.jsx
@@ -7,7 +7,6 @@ export default function Me()
     return(
         <div className=" pt-10 flex flex-col items-center ">
             <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" />
-
             <div className="flex flex-col items-center font-mont text-5xl text-neutral-content">
                 <p>Hello world, I'm Alex </p>
                 <p>a web developer</p>
@@ -15,8 +14,7 @@ export default function Me()
             <Link href="/#projects">
                 <div 
                     className="font-mont text-xl text-neutral-content bou px-4 py-2 rounded animate-bounce mt-10"
-                    style={{ width: 'auto' }}
-                >
+                    style={{ width: 'auto' }}>
                     my work ↓
                 </div>
             </Link>
--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -7,19 +7,15 @@ export default function Nav()
 
     return(  
     <div className="navbar">
-        <div className="flex-none menu menu-lg menu-horizontal px-5 text-base-content text-3l font-mont space-x-6 ">
+        <div className="flex-none menu menu-lg menu-horizontal px-5 text-base-content text-lg font-mont space-x-6 ">
         <span className = "p-2 md:p-4 rounded-lg hover:bg-gray-900 hover:cursor-pointer">
             <Link href = "/#projects">
-                <p className = "">
-                    Projects
-                </p>
+                Projects
             </Link>
         </span> 
         <span className = "p-2 md:p-4 rounded-lg hover:bg-gray-900 hover:cursor-pointer">
             <Link href = "/#about">
-                <p className = "">
-                    About
-                </p>
+                About
             </Link>
         </span>
         </div>
